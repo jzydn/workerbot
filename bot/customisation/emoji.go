2ea5f39921d04d38081be4d3b7eaa7cab698347c
jay <161174861+jzydn@users.noreply.github.com>
2025-05-10 00:30:47 +0100

    m

--- bot/customisation/emoji.go
+++ bot/customisation/emoji.go
@@ -2,8 +2,6 @@ package customisation
 
 import (
 	"fmt"
-
-	"github.com/TicketsBot-cloud/worker/config"
 	"github.com/rxdn/gdl/objects"
 	"github.com/rxdn/gdl/objects/guild/emoji"
 )
@@ -38,22 +36,22 @@ func (e CustomEmoji) BuildEmoji() *emoji.Emoji {
 }
 
 var (
-	EmojiId         = NewCustomEmoji("id", config.Conf.Emojis.Id, false)
-	EmojiOpen       = NewCustomEmoji("open", config.Conf.Emojis.Open, false)
-	EmojiOpenTime   = NewCustomEmoji("opentime", config.Conf.Emojis.OpenTime, false)
-	EmojiClose      = NewCustomEmoji("close", config.Conf.Emojis.Close, false)
-	EmojiCloseTime  = NewCustomEmoji("closetime", config.Conf.Emojis.CloseTime, false)
-	EmojiReason     = NewCustomEmoji("reason", config.Conf.Emojis.Reason, false)
-	EmojiSubject    = NewCustomEmoji("subject", config.Conf.Emojis.Subject, false)
-	EmojiTranscript = NewCustomEmoji("transcript", config.Conf.Emojis.Transcript, false)
-	EmojiClaim      = NewCustomEmoji("claim", config.Conf.Emojis.Claim, false)
-	EmojiPanel      = NewCustomEmoji("panel", config.Conf.Emojis.Panel, false)
-	EmojiRating     = NewCustomEmoji("rating", config.Conf.Emojis.Rating, false)
-	EmojiStaff      = NewCustomEmoji("staff", config.Conf.Emojis.Staff, false)
-	EmojiThread     = NewCustomEmoji("thread", config.Conf.Emojis.Thread, false)
-	EmojiBulletLine = NewCustomEmoji("bulletline", config.Conf.Emojis.BulletLine, false)
-	EmojiPatreon    = NewCustomEmoji("patreon", config.Conf.Emojis.Patreon, false)
-	EmojiDiscord    = NewCustomEmoji("discord", config.Conf.Emojis.Discord, false)
+	EmojiId         = NewCustomEmoji("id", 1339002097139453963, false)
+	EmojiOpen       = NewCustomEmoji("open", 1339002143729778730, false)
+	EmojiOpenTime   = NewCustomEmoji("opentime", 1339008285390405713, false)
+	EmojiClose      = NewCustomEmoji("close", 1339002174650187876, false)
+	EmojiCloseTime  = NewCustomEmoji("closetime", 1339008285390405713, false)
+	EmojiReason     = NewCustomEmoji("reason", 1339006380241321984, false)
+	EmojiSubject    = NewCustomEmoji("subject", 1013527369832738907, false)
+	EmojiTranscript = NewCustomEmoji("transcript", 1339007964018901103, false)
+	EmojiClaim      = NewCustomEmoji("claim", 1339004781766901935, false)
+	EmojiPanel      = NewCustomEmoji("panel", 1339004212222230640, false)
+	EmojiRating     = NewCustomEmoji("rating", 1339004985224200222, false)
+	EmojiStaff      = NewCustomEmoji("staff", 1339003554383527997, false)
+	EmojiThread     = NewCustomEmoji("thread", 1339003444098498561, false)
+	EmojiBulletLine = NewCustomEmoji("bulletline", 1339003248329228441, false)
+	EmojiPatreon    = NewCustomEmoji("patreon", 1339007402044817470, false)
+	EmojiDiscord    = NewCustomEmoji("discord", 1339002870657454091, false)
 	//EmojiTime       = NewCustomEmoji("time", 974006684622159952, false)
 )
 
